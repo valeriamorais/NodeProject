58a301d1c1968d2f378d85d5aef97bb2eb346a59
Valeria Morais <valeriacsmorais@gmail.com>
2020-05-18 12:03:21 -0300

    task2: jsx function

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,16 +2,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function getButtonText(){
+    return "Click on me"
+}
+
 // Create a react component
 const App = () =>{
-    const buttonText="Click Me"
 
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
             <button style={{ backgroundColor: 'blue', color: 'white' }}>
-            {buttonText}
+            {getButtonText()}
             </button>
         </div>
     );
